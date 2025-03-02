8f9709a7650bc4f0bbdfd43b2aedcbc1be854988
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-03-02 23:41:38 +0100

    add: array

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { useState, useEffect } from "react";
 
+const initialMovies = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [filteredMovies, setMovies] = useState(initialMovies)
 
   return (
     <>
